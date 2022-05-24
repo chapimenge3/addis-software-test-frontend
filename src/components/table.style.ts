6f316686f299199aa9a1629f85553f2a8b7f2b01
Chapi Menge <chapimenge3@gmail.com>
2022-05-25 01:51:30 +0300

    styled component added

---
+++ src/components/table.style.ts
@@ -0,0 +1,29 @@
+import styled from 'styled-components'
+
+export const ExployeesTable = styled.table`
+    font-family: Arial, Helvetica, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+`
+export const ExployeeData = styled.td`
+    border: 1px solid #ddd;
+    padding: 8px;
+`
+export const EmployeeHeader = styled.th`
+    border: 1px solid #ddd;
+    padding: 8px;
+    padding-top: 12px;
+    padding-bottom: 12px;
+    text-align: left;
+    background-color: #04AA6D;
+    color: white;
+`
+
+export const EmployeeRow = styled.tr`
+&:nth-child(even){
+    background-color: black;
+}
+&:hover{
+    background-color: grey;
+}
+`
