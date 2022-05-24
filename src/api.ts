a5f1a679a0f6bab1d3a5784a3cc974a6220acc36
Chapi Menge <chapimenge3@gmail.com>
2022-05-24 22:38:48 +0300

    project intialization with vite

---
+++ src/api.ts
@@ -0,0 +1,4 @@
+// Define baseurl const to https://628d249ca3fd714fd03ff793.mockapi.io/api/employee/Employee
+const BASE_URL = "https://628d249ca3fd714fd03ff793.mockapi.io/api/employee/Employee"
+
+export default {}
