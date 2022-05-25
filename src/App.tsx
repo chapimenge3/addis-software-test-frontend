fcbc04f2a4ec1014f78e473bd04645868c2a4e63
Chapi Menge <chapimenge3@gmail.com>
2022-05-25 10:29:22 +0300

    add: react-router to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,27 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import { useSelector, useDispatch } from 'react-redux'
-import { RootState } from './redux/store'
-import { EmployeeHeader, EmployeeRow, ExployeeData, ExployeesTable } from './components/table.style'
+import "./App.css";
+import Homepage from "./Homepage";
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+} from "react-router-dom";
+import EmployeeDetails from './EmployeeDetail'
 
 function App() {
-  const dispatch = useDispatch()
-  const { loading, employees, error } = useSelector((state: RootState) => state.employee)
-
-  useEffect(() => {
-    console.log('useEffect ran', employees);
-    dispatch({ type: 'employee/getEmployeesStart' })
-  }, []);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Welcome to Addis Software</p>
-        <p>
-          {/* Show employee details for all of the employees */}
-          {loading ? 'Loading...' : error ? error:'' }
-          { !loading && employees ?  <ExployeesTable>
-            <EmployeeRow>
-              <EmployeeHeader>ID</EmployeeHeader>
-              <EmployeeHeader>Name</EmployeeHeader>
-              <EmployeeHeader>Salary</EmployeeHeader>
-              <EmployeeHeader>Gender</EmployeeHeader>
-              <EmployeeHeader>Birth Date</EmployeeHeader>
-            </EmployeeRow>
-            { employees.map(emplyee => <EmployeeRow>
-              <ExployeeData>{emplyee.id}</ExployeeData>
-              <ExployeeData>{emplyee.name}</ExployeeData>
-              <ExployeeData>{emplyee.salary}</ExployeeData>
-              <ExployeeData>{emplyee.gender}</ExployeeData>
-              <ExployeeData>{emplyee.birthdate}</ExployeeData>
-            </EmployeeRow> ) }
-          </ExployeesTable>: '' }
-          {/* {loading ? 'Loading...' : error ? 'Error' : employees.map(employee => employee.name)} */}
-        </p>
-      </header>
-    </div>
-  )
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <Homepage></Homepage>
+          </Route>
+          <Route path="/employee" exact>
+          <EmployeeDetails></EmployeeDetails>
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
